c535263e87a6c596d4ae9df4622d18d397bb8479
Dan <1770421128@qq.com>
2015-06-24 09:56:29 +0800

    新增一个文件

---
+++ LanGit/Myview.h
@@ -0,0 +1,13 @@
+//
+//  Myview.h
+//  LanGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Myview : UIView
+
+@end
---
+++ LanGit/Myview.m
@@ -0,0 +1,21 @@
+//
+//  Myview.m
+//  LanGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanouhn. All rights reserved.
+//
+
+#import "Myview.h"
+
+@implementation Myview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
